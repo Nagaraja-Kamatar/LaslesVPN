4cd48c92f93704caed1620e22db97af6a0d28c3e
Nagaraja Kamatar <145822414+Nagaraja-Kamatar@users.noreply.github.com>
2024-03-01 18:59:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const indexes = document.querySelectorAll('.indexes li');
-const tabs = document.querySelectorAll('.tab');
-const contents = document.querySelectorAll('.tab-content');
-
-function reset() {
-  for (let i = 0; i < tabs.length; i++) {
-    indexes[i].style.borderColor = 'transparent';
-    tabs[i].style.zIndex = 0;
-    tabs[i].classList.remove('active');
-    contents[i].classList.remove('active');
-  }
-}
 
